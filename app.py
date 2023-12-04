fb3cfffe21e5ba325193150e39773ec329852c9e
Priya <priya@t.digital>
2023-12-05 01:03:52 +0530

    fixie addon

--- app.py
+++ app.py
@@ -6,16 +6,16 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello():
-    return 'Hello from Python!'
-
-@app.route('/next')
-def next():
+    # return 'Hello from Python!'
     proxyDict = {
               "http"  : os.environ.get('FIXIE_URL', ''),
               "https" : os.environ.get('FIXIE_URL', '')
             }
-    r = requests.get('http://fixie:YpcpuxmrFDMenhX@velodrome.usefixie.com:80', proxies=proxyDict)
+    r = requests.get('http://fixie:YpcpuxmrFDMenhX@54.173.229.200:80', proxies=proxyDict)
     return 'From Fixie'
+
+# @app.route('/next')
+# def next():
 
 if __name__ == '__main__':
    app.run(debug=True)
