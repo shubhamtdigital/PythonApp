28b1380b511dba5eb12f185db8349221a2036b98
priyatdigital <151191558+priyatdigital@users.noreply.github.com>
2023-12-05 21:45:15 +0530

    Update app.py

--- app.py
+++ app.py
@@ -10,8 +10,8 @@ import socket
 app = Flask(__name__)
 
 proxyDict = {
-              "http"  : os.environ.get('https://r09cndoizux678:44m6nmtadw9bg10eowfxi45cyrbyku@us-east-shield-04.quotaguard.com:9294', ''),
-              "https" : os.environ.get('https://r09cndoizux678:44m6nmtadw9bg10eowfxi45cyrbyku@us-east-shield-04.quotaguard.com:9294', '')
+              "http"  : 'https://r09cndoizux678:44m6nmtadw9bg10eowfxi45cyrbyku@us-east-shield-04.quotaguard.com:9294',
+              "https" : 'https://r09cndoizux678:44m6nmtadw9bg10eowfxi45cyrbyku@us-east-shield-04.quotaguard.com:9294'
  }
 
 @app.route('/')
