ee10ad6a837d011a0f14927038c9a7b2228c7d62
shubhamtdigital <150453663+shubhamtdigital@users.noreply.github.com>
2023-12-06 01:31:23 +0530

    Update app.py

--- app.py
+++ app.py
@@ -49,54 +49,42 @@ def connect():
     my_ip = get_public_ip()
     print(f"My public IP address is: {my_ip}")
     # Parse the database URL
-    db_url = "postgres://ucrol25emqd2ch:p31d791a3fe8bcb5b5102d7b8b43f08ca70ee2cc9d7943c23b4db6b110324346e@ec2-52-2-248-148.compute-1.amazonaws.com:5432/d2r45oj3jf7gs7"
-    url = urlparse(db_url)
-    # Set up the proxy settings
-    print(proxyDict)
-    proxy_host = proxyDict['http']
-    # proxy_ips = ['54.173.229.200', '54.175.230.252']
-    proxy_ips = ['54.160.232.145', '44.215.229.88']
-    proxy_port = 1080
-    proxy_type = socks.SOCKS5  # Change this based on your proxy type
-
-    selected_proxy_ip = proxy_ips[0]
-    # Set up the connection parameters
-    conn_params = {
-        'database': url.path[1:],
-        'user': url.username,
-        'password': url.password,
-        'host': url.hostname,
-        'port': url.port,
-        'sslmode': 'require',  # Use 'require' to enable SSL
-        'sslcert': '/postgresql.crt',  # Path to client certificate file
-        'sslkey': '/postgresql.key'  # Pat
-    }
-
-    # Set up the proxy
-    socks.set_default_proxy(proxy_type, addr=proxy_host)
+    database_url = "postgres://ucrol25emqd2ch:p31d791a3fe8bcb5b5102d7b8b43f08ca70ee2cc9d7943c23b4db6b110324346e@ec2-52-2-248-148.compute-1.amazonaws.com:5432/d2r45oj3jf7gs7"
+    url = urlparse(database_url)
+    
+    # Set up SOCKS proxy
+    socks_host = "fixie:k2KJ04rPQQ047n8@speedway.usefixie.com"
+    socks_port = 1080  # Change to the actual port used by your SOCKS proxy
+    
+    socks.setdefaultproxy(socks.PROXY_TYPE_SOCKS5, socks_host, socks_port)
     socket.socket = socks.socksocket
-
-    # Connect to the database via proxy
+    
+    # Connect to PostgreSQL
     try:
-        connection = psycopg2.connect(**conn_params)
+        connection = psycopg2.connect(
+            database=url.path[1:],
+            user=url.username,
+            password=url.password,
+            host=url.hostname,
+            port=url.port
+        )
+    
+        # Create a cursor
         cursor = connection.cursor()
-
-        # Example query
-        query = sql.SQL('SELECT * FROM pgadmin."Prospect" limit 1;')
-        cursor.execute(query)
-
-        # Fetch results
-        results = cursor.fetchall()
-        print('results:', results)
-        return "private_working"
-
+    
+        # Execute your SQL queries here
+    
+        # Commit changes
+        connection.commit()
+    
     except Exception as e:
         print(f"Error: {e}")
-
-    # finally:
-    #     if connection:
-    #         cursor.close()
-    #         connection.close()
+    
+    finally:
+        # Close the cursor and connection
+        if connection:
+            cursor.close()
+            connection.close()
 
 @app.route('/new')
 def newRoute():
